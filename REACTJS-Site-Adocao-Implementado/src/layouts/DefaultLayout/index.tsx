8608b41976b855d80506d670ac4ed519ae87c211
Lucas <lu_oliveira@id.uff.br>
2023-07-17 13:47:20 -0300

    nunca tem fim

--- REACTJS-Site-Adocao-Implementado/src/layouts/DefaultLayout/index.tsx
+++ REACTJS-Site-Adocao-Implementado/src/layouts/DefaultLayout/index.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
+import ScrollToTop from "../../components/ScrollToTop";
 
 export function DefaultLayout() {
     return(
         <>
+        <ScrollToTop />
         <Header />
         <Outlet />
         <Footer />
