a6adbd2a43828805a29e20c3c0cf1979cdb7681a
Lucas Souza de Oliveira <102678242+lucassdouff@users.noreply.github.com>
2023-11-14 17:54:09 -0300

    Delete API-RESTful-SB-Site-Adocao-Implementado directory

--- API-RESTful-SB-Site-Adocao-Implementado/src/main/java/com/lucasoliveira/exception/EntidadeNaoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.lucasoliveira.exception;
-
-public class EntidadeNaoEncontradaException extends RuntimeException {
-    public EntidadeNaoEncontradaException(String message) {
-        super(message);
-    }
-}
--- API-RESTful-SB-Site-Adocao-Implementado/src/main/java/com/lucasoliveira/repository/CategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lucasoliveira.repository;
-
-import com.lucasoliveira.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
-}
