5dea9fcda07bca4178e689ad4d6f958e2ddb2800
Lucas <lucas.souza0673@gmail.com>
2023-11-27 19:04:19 -0300

    update cadastro gatos

--- REACTJS-Site-Adocao-Implementado/src/pages/ParaAdocao/index.tsx
+++ REACTJS-Site-Adocao-Implementado/src/pages/ParaAdocao/index.tsx
@@ -1,13 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import { CadastroDeGatosForm } from "../../components/CadastroDeGatosForm";
 
 export default function ParaAdocao() {
+
+    const user = JSON.parse(localStorage.getItem('session') || '{}');
+
+    const navigate = useNavigate();
+    
     return(
         <div className="container mt-2 mb-auto pb-2 h-100">
             <div className="mb-4 mt-4">
                 <h5>Cadastro de gatos para adoção</h5>
             </div>
             
-            <CadastroDeGatosForm />
+            {
+                user.id ? (
+                    <CadastroDeGatosForm />
+                ) : (
+                    <button className="btn btn-dark" onClick={() => navigate('/login')}>Fazer login</button>
+                )
+            }
         </div>
     )
 }
